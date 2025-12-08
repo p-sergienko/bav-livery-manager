024365666288db2934097eb316c3e54c0921c742
P-Sergienko <psergienko40@gmail.com>
2025-12-08 22:01:28 +0300

    update constants to use bav domain

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,4 +1,14 @@
-const PANEL_BASE_URL = (typeof process !== 'undefined' && process.env.BAV_PANEL_BASE_URL ? process.env.BAV_PANEL_BASE_URL : 'http://localhost:3000').replace(/\/$/, '');
+const envIsProduction = typeof process !== 'undefined' && (process.env.NODE_ENV === 'production' || process.env.BUN_ENV === 'production');
+
+const rawPanelBase = (() => {
+	if (!envIsProduction) {
+		return 'http://localhost:3000';
+	}
+
+	return (typeof process !== 'undefined' && process.env.BAV_PANEL_BASE_URL) || 'https://liveries.bavirtual.co.uk';
+})();
+
+export const PANEL_BASE_URL = rawPanelBase.replace(/\/$/, '');
 
 export const REMOTE_LIVERY_LIST_URL = `${PANEL_BASE_URL}/api/simulator/liveries`;
 export const PANEL_BASE_ENDPOINT = PANEL_BASE_URL;
