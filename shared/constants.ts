6628ab6560266b0c751a9a105f989b17ed5d1b1d
P-Sergienko <psergienko40@gmail.com>
2025-12-09 21:43:04 +0300

    fix the constant for prod

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,5 +1,5 @@
-// export const PANEL_BASE_URL = 'https://liveries.bavirtual.co.uk';
-export const PANEL_BASE_URL = 'http://localhost:3000';
+export const PANEL_BASE_URL = 'https://liveries.bavirtual.co.uk';
+// export const PANEL_BASE_URL = 'http://localhost:3000';
 
 export const REMOTE_LIVERY_LIST_URL = `${PANEL_BASE_URL}/api/simulator/liveries`;
 export const PANEL_BASE_ENDPOINT = PANEL_BASE_URL;
